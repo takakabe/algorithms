dc795f1a9cc5559c1f7fb0798798b3ab1e839463
takakabe <takakabe5384@gmail.com>
2021-03-16 23:48:45 +0900

    fix

--- recursive_call/binary_search.py
+++ recursive_call/binary_search.py
@@ -2,15 +2,12 @@ def binary_search(max_index, min_index):
   mid_index = (max_index + min_index) // 2
   if array[mid_index] == value:
     return(mid_index)
-  if value < array[mid_index]:
-    max_index = mid_index - 1
-  else:
-    min_index = mid_index + 1
-  return(binary_search(max_index, min_index))
+  return(binary_search(mid_index - 1, min_index) \
+    if value < array[mid_index] else binary_search(max_index, mid_index + 1))
 
 if __name__ == '__main__':
   array = [1,3,5,11,12,13,17,22,25,28,]
-  value = 5
+  value = 28
   min_index = 0
   max_index = len(array) - 1
 
